d3c435a056a88b6421ef2ee066c622a31c0d657d
amyard <maksymturenko@gmail.com>
2019-06-06 11:01:18 +0300

    ordersitem model

--- core/orders/admin.py
+++ core/orders/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from core.orders.models import Order
+from core.orders.models import Order, OrderItem
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'number', 'created_date',)
+
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order_id', 'product_name', 'product_price', 'amount', )
+    search_fields = ['product_name']
+    list_filter = ['product_name']
--- core/orders/models.py
+++ core/orders/models.py
@@ -3,13 +3,14 @@ from django.utils.translation import ugettext_lazy as _
 
 from core.common.utils import random_date
 
+import random
 
 class Order(models.Model):
-    number = models.PositiveIntegerField(_('Number'), blank=True, default=' ')
+    number = models.PositiveIntegerField(_('Number'), blank=True, default='')
     created_date = models.DateTimeField(_('Created Date'), default=random_date, blank=True)
 
     def __str__(self):
-        return f'{self.number} was created {self.created_date}'
+        return f'{self.number} was created {self.created_date.strftime("%Y-%m-%d %H:%M")}'
 
     class Meta:
         ordering = ['-pk']
@@ -17,3 +18,23 @@ class Order(models.Model):
     def save(self, *args, **kwargs):
         self.number = Order.objects.all().count()+1
         super(Order, self).save(*args, **kwargs)
+
+
+
+class OrderItem(models.Model):
+    order_id=models.ForeignKey(Order, on_delete = models.CASCADE, related_name='orders')
+    product_name=models.CharField(_('Product'), max_length=55, blank=True)
+    product_price=models.PositiveIntegerField(_('Price'), blank=True)
+    amount=models.PositiveIntegerField(_('Amount'), blank=True)
+
+    def __str__(self):
+        return f'{self.order_id.number}-{self.product_name}'
+
+    class Meta:
+        ordering = ['-pk']
+
+    def save(self, *args, **kwargs):
+        self.product_name = f'Товар-{random.randint(1, 50)}'
+        self.product_price = random.randint(100, 9999)
+        self.amount = random.randint(1, 10)
+        super(OrderItem, self).save(*args, **kwargs)
